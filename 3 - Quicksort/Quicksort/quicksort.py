abf91e95be2d5a59dfd9b0703138e0583348fa3e
amnghd <32374722+amnghd@users.noreply.github.com>
2018-04-10 11:26:32 -0700

    adding quicksort

--- 3 - Quicksort/Quicksort/quicksort.py
+++ 3 - Quicksort/Quicksort/quicksort.py
@@ -1,27 +1,36 @@
-input_file = 'QuickSort.txt'
+input_file = 'QuickSort.txt' # input file that has a list of integers
 
-with open(input_file, 'r') as data:
+counter = 0
+with open(input_file, 'r') as data: # accessing the integers in the data
     line = data.read().split("\n")
     integers = list(map(int, line))
 
+
+
 def partition(A,l):
-    l = 0 # pivot point
+    global counter
     r = len(A)
-    if r ==1:
+    pivot = A[l]
+    i = 1
+    for j in range(l+1, r):
+        counter += 1
+        if A[j] < pivot:
+            A[j], A[i] = A[i], A[j] # swaping in case comparison hold true
+            i += 1
+    A[l], A[i-1] = A[i-1], A[l] # moving the pivot element
+    return i-1 # note, although we are returning i-1, we have changed A in place
+
+
+def QuickSort(A):
+    if len(A) <= 1:
         return A
-    elif r > 1:
-        pivot = A[l]
-        i = 1
-        for j in range(l+1, r):
-            if A[j] < pivot:
-                A[j], A[i] = A[i], A[j]
-                i += 1
-        A[l], A[i-1] = A[i-1], A[l]
-        left = A[:i-1]
-        right = A[i:]
-        sorted_left = [].extend(partition(left, 0))
-        sorted_right = [].extend(partition(right, 0))
+    else:
+        l = 0
+        m = partition(A, l)
+        left = QuickSort(A[:m])
+        right = QuickSort(A[m+1:])
+        A[:m], A[m+1:] = left, right
+        return A
 
-        return sorted_left.append(pivot).append(sorted_right)
-
-print (partition ([2,3],0))
+print(QuickSort(integers))
+print(counter)
